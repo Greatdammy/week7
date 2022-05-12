9a445cbe6e0efc2afbe557f0bd3cd0367da49137
DAMILARE OBAFEMI <damimmanuel7@gmail.com>
2022-05-12 09:42:28 +0100

    calsswork on threading

---
+++ week7.academy/Account.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7.academy
+{
+    public class Account
+    {
+          public int id { get; set; }
+            public string Name { get; set; }
+            public string Gender{ get; set; }
+            public decimal Amount { get; set; }
+            public string Yearofspecification { get; set; }
+
+            public static List<Account> GetAccounts()
+            {
+                return new List<Account>
+                {
+                    new Account { id = 2, Name = "Amar", Gender = "F", Amount = 400000, Yearofspecification = "2 years"},
+                    new Account { id = 6, Name = "Joe", Gender = "M", Amount = 40000, Yearofspecification = "2 months"},
+                    new Account { id = 8, Name = "Tola", Gender = "F", Amount = 800000, Yearofspecification = "5 days"},
+                    new Account { id = 10, Name = "Jude", Gender = "M", Amount = 500000, Yearofspecification = "6 years"},
+                    new Account { id = 12, Name = "Amir", Gender = "F", Amount = 1000000, Yearofspecification = "1 years"},
+                    new Account { id = 14, Name = "Sam", Gender = "F", Amount = 600000, Yearofspecification = "1 years"},
+                    new Account { id = 16, Name = "Joy", Gender = "F", Amount = 100000, Yearofspecification = "3 years"},
+                    new Account { id = 18, Name = "Ola", Gender = "M", Amount = 20000, Yearofspecification = "4 months"}
+                };
+
+            }
+
+    }
+        public class week
+        {
+        public string Days { get; set; }
+
+             public static List<week> GetWeeks()
+             {
+               return new List<week>
+               {
+                    new week {Days = "SUNDAY"},
+                    new week {Days = "MONDAY"},
+                    new week {Days = "TUESDAY"},
+                    new week {Days = "WEDNESDAY"},
+                    new week {Days = "THURSDAY"},
+                    new week {Days = "FRIDAY"},
+                    new week {Days = "SATURDAY"}
+               };
+             }
+        }
+    public class year
+    {
+        public string Months { get; set; }
+
+        public static List<year> GetYears()
+        {
+            return new List<year>
+            {
+                new year {Months = "JANUARY"},
+                new year {Months = "FEB"},
+                new year {Months = "MARCH"},
+                new year {Months = "APRIL"},
+                new year {Months = "MAY"},
+                new year {Months = "JUNE"},
+                new year {Months = "JULY"},
+                new year {Months = "AUGUST"},
+                new year {Months = "SEPTEMBER"},
+                new year {Months = "OCTOBER"},
+                new year {Months = "NOVEMBER"},
+                new year {Months = "DECEMBER"},
+
+            };
+        }
+    }
+}
--- week7.academy/Program.cs
+++ week7.academy/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,35 +12,63 @@ namespace week7.academy
     {
         public static void Main()
         {
-            var total = calculator.Add(50, 50);
-            Console.WriteLine(total);
-            var subtotal = calculator.Add(new List<int> { 5, 6, 7, 8, 4, 6, 78, 3, 5, 2, 300 });
-            Console.WriteLine(subtotal);
+            var accounts = Account.GetAccounts();
+
+
+            var sum = accounts.Select(x => x.Amount )                            
+                               .Sum(x => x);
+            Task.Run(() => Console.WriteLine($"Total amount: {sum}"));
+            Console.WriteLine();
+
+            
+            var list = accounts.Select(x => x.Name)
+                                .OrderBy(x => x)
+                                .ToList();
+            foreach(var names in list)
+            {
+                Task.Run(() => Console.WriteLine(names));
+            }
+
+            Console.ReadKey();
+            Console.WriteLine();
+
+            var days = week.GetWeeks();
+
+            var item = days.Select(x => x.Days)
+                            .OrderBy(x => x);
+            Console.WriteLine("Days of the week:");
+            foreach(var day in item)
+            {
+                Console.WriteLine(day);
+            }
+            Console.WriteLine();
+
+
+            var mont = year.GetYears();
+
+            var item2 = mont.Select(x => x.Months)
+                            .OrderBy(x =>x);
+            Console.WriteLine("Months of the year:");
+            foreach( var month in item2)
+            {
+                Console.WriteLine(month);
+            }
+
+            Console.WriteLine();
+
+            var items = item2.Select(x => x)
+                             .Reverse().Take(2)
+                             .OrderBy(x => x);
+            foreach( var item1 in items)
+            {
+                Console.WriteLine(item1);
+            }
+
         }
 
-
-        public class calculator
-        {
-            [Obsolete("use the second method")]
-            // using lamda expression as againt the normal method 
-           // public static int Add(int firstnumber, int secondnumber) => firstnumber + secondnumber;
-
-            public static int Add(int firstnumber, int secondnumber)
-            {
-                Console.WriteLine("method outdated");
-                return firstnumber + secondnumber; 
-            }
-            public static int Add(List<int> numbers)
-            {
-                int result = 0;
-                foreach(var number in numbers)
-                {
-                    result = result + number;
-                }
-                return result;
-
-            }
-        }
+        
+      
+       
 
     }
 }
